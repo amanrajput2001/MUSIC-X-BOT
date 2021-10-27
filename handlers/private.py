a2e3923be15b78d30af9382eac401b438ec30b51
amanrajput2001 <90300203+amanrajput2001@users.noreply.github.com>
2021-10-27 19:17:11 +0530

    Add files via upload

---
+++ handlers/private.py
@@ -0,0 +1,47 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+from helpers.filters import other_filters2
+
+
+@Client.on_message(other_filters2)
+async def start(_, message: Message):
+    
+    await message.reply_text(
+        f"""**
+🌠𝗧𝗵𝗶𝘀 𝗜𝘀 𝗔𝗱𝘃𝗮𝗻𝗰𝗲 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗠𝘂𝘀𝗶𝗰 𝗕𝗼𝘁 \n🌺𝗥𝘂𝗻 𝗢𝗻 𝗣𝗿𝗶𝘃𝗮𝘁𝗲 𝗩𝗣𝗦 𝗦𝗲𝗿𝘃𝗲𝗿 \n🌼𝗙𝗲𝗲𝗹 𝗛𝗶𝗴𝗵 𝗤𝘂𝗮𝗹𝗶𝘁𝘆 𝗠𝘂𝘀𝗶𝗰 𝗜𝗻 𝐕𝐂 𝐝𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐛𝐲 @itsamanrajput
+        """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "❰𝗢𝘄𝗻𝗲𝗿❱", url=https://t.me/itsamanrajput")
+                  ],[
+                    InlineKeyboardButton(
+                        "❰𝗦𝘂𝗽𝗽𝗼𝗿𝘁❱", url="https://t.me/bestfrnsforever"
+                    ),
+                    InlineKeyboardButton(
+                        "❰𝗚𝗿𝗼𝘂𝗽❱", url="https://t.me/bestfrnsforever"
+                    )
+                ],[ 
+                    InlineKeyboardButton(
+                        "❰𝗦𝗼𝘂𝗿𝗰𝗲❱", url="https://municipa.li/"
+                    )]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("hexor") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text("""**𝐌𝐔𝐒𝐈𝐂 𝐁𝐎𝐓 𝐎𝐍𝐋𝐈𝐍𝐄 𝐇𝐀𝐈 𝐁𝐄\n🌠𝐀𝐌𝐀𝐍 𝐑𝐀𝐉𝐏𝐔𝐓 𝐎𝐏 <3**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌼𝗦𝘂𝗽𝗽𝗼𝗿𝘁", url="https://t.me/bestfrnsforever")
+                ]
+            ]
+        )
+   )
